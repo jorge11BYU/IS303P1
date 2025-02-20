06eb90990ebc561790de7e1a35813d294cbe587e
iakoba-lee <137527733+iakoba-lee@users.noreply.github.com>
2025-02-20 10:28:06 -0700

    Created a function to multiply two numbers
    This takes two #s and multiplies them. The custom function is called multiplyTwo Numbers()

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Jacob Lee
+# Feb 20, 2025
+# This is a function that multiplies two numbers together
+
+# This function takes two imputted numbers and multiplies them together
+def multiplyTwoNumbers(iNum1, iNum2):
+    iTotal = iNum1 * iNum2
+    return iTotal
+
